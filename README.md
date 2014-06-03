7a981c861c3b46953f9cfdd944ef1547cd09737d
Robert Moss <mossr@wit.edu>
2014-06-02 23:24:01 -0400

    README.md updated, NEW: unit_conversions.jl

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
 SI.jl
 =============
 
-- **[Robert Moss](mailto:rmoss92@gmail.com)** <sup>(1)</sup>
+A Julia library for unit types validated unit conversions.
 
-<sup>(1) Boston, MA</sup>
 
-## Unit Types
+- **[Robert Moss](mailto:rmoss92@gmail.com)**
+
+### Initialization
+
+Load the library via ```require("SI.jl")``` then display the available types and explicit conversions with ```showSI()``` .
+
+
+<sup>**Note:**</sup>  
+<sup>Implicit types and conversions are available as long as each unit is defined within SI.jl</sup>  
+<sup>**Example**:</sup>  
+<sup>The type ```m/s``` is not defined, but both ```m``` (meters) and ```s``` (seconds) are defined.</sup>
+
+### Unit Types
+	
+	distance = SI(90.2, :km) # kilometers
+	time = SI(4.2, :s) # seconds
+
+### Unit Arithmetic
+
+	velocity = distance/time
+	
+	SI(21.476190476190474,:km/s,"kilometers per second",:velocity)
+
+
+### Unit Conversion
+
+	velocity >> symbol("m/s") # use symbol function for units containing '/'
+	
+	SI(21476.190079180236,:m/s,"meters per second",:velocity)
+
+### User defined unit conversions
+
+To add your own unit conversions to the ```unit_conversions.jl``` file.
+
+##### Nomenclature for user defined unit conversions  
+1. Use the macro ```@conversion``` to include the user defined function within ```showSI()```.  
+2. Use the style of ```this_to_that(this)``` function names.
--- SI.jl
+++ SI.jl
@@ -1,4 +1,5 @@
-include("conversion.jl")
+reload("conversion.jl")
+reload("unit_conversions.jl")
 
 type SI
 	value::Float64
@@ -28,6 +29,7 @@ end
 #
 # Operator overloading
 #
+
 function *(m1::SI, m2::SI)
 	if m1.unit_type != m2.unit_type
 		error("The unit types do not match; ", m1.unit_type, " and ", m2.unit_type,".")
@@ -47,8 +49,11 @@ function /(m1::SI, m2::SI)
 end
 
 #
-# Uses:	siUnit >> :m 	# Converts the siUnit to meters (returns the changed siUnit)
-#			siUnit >>= :m 	# Converts the siUnit to meters (alters the current "from::SI")
+# Uses:	
+#
+#	siUnit >> :m 	# Converts the siUnit to meters (returns the changed siUnit)
+#	siUnit >>= :m 	# Converts the siUnit to meters (modifies the current siUnit)
+
 >>(from::SI, to_sym::Symbol) = return to(from, to_sym)
 
 function showSI(pretty_print=true)
@@ -168,34 +173,3 @@ function to(input::SI, new_unit::Symbol)
 		error("Undefined function ", func)
 	end
 end
-
-###### LENGTH UNIT CONVERIONS ######
-@conversion kpc_to_km(kpc) = return kpc * 3.08567758 * float32(10)^16
-@conversion kpc_to_m(kpc) = return kpc * 3.08567758 * float32(10)^19
-@conversion kpc_to_cm(kpc) = return m_to_cm(kpc_to_m(kpc))
-
-@conversion km_to_kpc(km) = return km * 3.24077929 * float32(10)^-17
-@conversion km_to_m(km) = return cm_to_m(km_to_cm(km))
-@conversion km_to_cm(km) = return kpc_to_cm(km_to_kpc(km))
-
-@conversion m_to_kpc(m) = return m * 3.24077929 * float32(10)^-20
-@conversion m_to_km(m) = return m * 0.001
-@conversion m_to_cm(m) = return m * 100
-
-@conversion cm_to_kpc(cm) = return km_to_kpc(cm_to_km(cm))
-@conversion cm_to_km(cm) = return m_to_km(cm_to_m(cm))
-@conversion cm_to_m(cm) = return cm * 0.01
-
-###### TIME UNIT CONVERIONS ######
-@conversion s_to_min(s) = return s/60
-@conversion s_to_hr(s) = return s_to_min(s)/60
-
-@conversion min_to_s(min) = return min*60
-@conversion min_to_hr(min) = return min/60
-
-@conversion hr_to_min(hr) = return hr*60
-@conversion hr_to_s(hr) = return hr_to_min(hr)*60
-
-###### DENSITY UNIT CONVERIONS ######
-###### GeV/cm^3 to kg/km*s^2 
-@conversion GeVcm3_to_kgkms2(GeV) = return GeV * 0.1602117
